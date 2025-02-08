07b6a352e0a32f98b765a025afe89f8dfc50bef2
Harshal Bhute <harshalbhute39@gmail.com>
2025-02-08 13:23:33 +0530

    Modified

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Welcome = (props) => {
   return (
     <>
-    <h1>Hey! {props.name}</h1>
+    <h1>Hey {props.name}!</h1>
     <h2>Welcome to School</h2>
     <p>We are excited to have you here. Let's make this a great learning journey!</p>
     </>
