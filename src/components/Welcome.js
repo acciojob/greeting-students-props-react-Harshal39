e96c92a525a2402d5736205a101a98e8c0a81e7d
Harshal Bhute <harshalbhute39@gmail.com>
2025-02-08 13:11:42 +0530

    Commited

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,14 @@
 // write code for Welcome component here
+import React from 'react'
+
+const Welcome = (props) => {
+  return (
+    <>
+    <h1>Hey! {props.name}</h1>
+    <h2>Welcome to School</h2>
+    <p>We are excited to have you here. Let's make this a great learning journey!</p>
+    </>
+  )
+}
+
+export default Welcome
